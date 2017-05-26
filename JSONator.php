2ab08ed9ea18a614434a77d9ce15cd03f715107f
Your Name <you@example.com>
2017-05-26 15:38:48 -0600

    finished crud for collections

--- JSONator.php
+++ JSONator.php
@@ -6,10 +6,6 @@ class FileNator
 	{
 		$this->filePath = $filePath;
 	}
-	public function updateFilePath($filePath)
-	{
-		$this->filePath = $filePath;
-	}
 	public function getJSONfromFile()
 	{
 		$filePath = $this->filePath;
@@ -28,10 +24,9 @@ class FileNator
 	}
 	public function writeJSONtoFile($json)
 	{
-		$file = fopen('results.json', 'w')
+		$filePath = $this->filePath;
+		$file = fopen($filePath, 'w')
 		    	or exit("Unable to open file ($filePath)");
-
-
 		fwrite($file, json_encode($json));
 		fclose($file);
 	}
@@ -48,42 +43,108 @@ class JSONator
 		$this->JSON = $this->fileNator->getJSONfromFile();
 		$this->schema = $schema;
 	}
+	private function reloadJSONfromFile()
+	{
+		$this->JSON = $this->fileNator->getJSONfromFile();
+	}
     public function read($type)
     {
     	return $json;
     }
-    public function write($type, $record)
+    public function write($record)
     {
-    	echo $this->validateJSON($type, $this->JSON) ? 'true' : 'false';
+    	if($this->validateJSON($record))
+    	{
+    		$newRecordID;
+    		$this->reloadJSONfromFile();
+    		if(count($this->JSON[0]) != 0)
+    		{
+    			$newRecordID = $this->JSON[count($this->JSON) - 1]['id'] + 1; //get last ID in collection and set the new record's ID to last ID plus one.
+    		}
+    		else
+    		{
+    			array_pop($this->JSON); //remove default array when file is empty.
+    			$newRecordID = 1;
+    		}
+    		
+    		$record['id'] = $newRecordID;
+    		array_push($this->JSON, $record);
+    		$this->fileNator->writeJSONtoFile($this->JSON);
+    		
+    	}
+    	else
+    	{
+    		return false;
+    	}
+    	$this->reloadJSONfromFile(); //reload new collection are return.
+    	return $this->JSON;
     }
-    public function update($type, $id, $record)
+    public function update($id, $record)
     {
+    	$indexToUpdate;
+    	if($this->validateJSON($record))
+    	{
+    		$this->reloadJSONfromFile();
+
+    		foreach(array_values($this->JSON) as $i => $val) //find record in collection to update.
+    		{
+    			if($id == $val['id']) //if id is found update values of record found in collection.
+    			{
+    				foreach($record as $key => $val1) 
+    				{
+    					$this->JSON[$i][$key] = $val1;
+    					break;
+    				}
+    			}
+    		}
+
+    		$this->fileNator->writeJSONtoFile($this->JSON);
+    		$this->reloadJSONfromFile();
+
+    	}
+    	else
+    	{
+    		return false;
+    	}
     }
-    public function delete($type, $id)
+    public function delete($id)
     {
+    	foreach(array_values($this->JSON) as $i => $val)
+    	{
+    		if($id == $val['id'])
+    		{
+    			unset($this->JSON[$i]);
+    			$this->JSON = array_values($this->JSON);
+    			$this->fileNator->writeJSONtoFile($this->JSON);
+    			$this->reloadJSONfromFile();
+    			
+    			return $this->JSON;
+    		}
+    	}
+    	return false;
+
     }
     public function printSomeShit()
     {
-    	$this->write('u', []);
+    	var_dump($this->fetchRecordById(4));
     }
-    public function fetchRecordById($type, $id)
+    public function fetchRecordById($id)
 	{
+		$this->reloadJSONfromFile();
 		foreach ($this->JSON as $key => $value) 
 		{
 			if($id == $value['id'])
 			{
-				return $json[$key];
+				return $this->JSON[$key];
 			}
 		}
-		return;
+		return false;
 	}
 
-	public function validateJSON($type, $json)
+	public function validateJSON($json)
 	{
-		$json = $json[0]; //TODO: CHANGE THIS LINE TO ACCOMODATE REAL FLOW
 		$valid = true;
     	$schema = $this->schema;
-    	var_dump($json);
 		if(is_array($json))
 		{
 			foreach($json as $key => $val)
@@ -121,14 +182,12 @@ class Reservation extends Model
 	{
 		$this->depart = $json['depart'];
 		$this->return = $json['return'];
-		
 	}
 	public static function jsonSchema() 
 	{
         return array(
         	"depart" => "string",
         	"return" => "string",
-        	"id" => "integer",
         	"reservation" => "array"
         	);
     }
@@ -147,7 +206,6 @@ class User extends Model
         return array(
         	"first" => "string",
         	"last" => "string",
-        	"id" => "integer",
         	"reservation" => "array"
         	);
     }
